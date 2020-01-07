5178a711253bbd4ae8c9f010648a0fca18f6643c
unknown <kailashkumar5397638@gmail.com>
2020-01-07 20:53:45 +0500

    checkout

--- src/Config/Router/router.js
+++ src/Config/Router/router.js
@@ -6,16 +6,19 @@ import {
 } from "react-router-dom";
 
 import {
-    Home
+    Home,
 }
 from './../../Containers'
-
+import Checkout from "./../../Containers/Checkout/Checkout"
 
 export default class ReactRouter extends React.Component{
     render(){
         return(
             <Router>
                 <Route exact path="/" component={Home} />
+                <Route exact path="/checkout" component={Checkout} />
+
+                
             </Router>
         )
     }
