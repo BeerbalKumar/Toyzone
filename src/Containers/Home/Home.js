410d61fc4498458e606873ba5f9f403c20f3ae0f
BeerbalKumar <iambeerbalkumar@gmail.com>
2020-01-07 20:15:14 +0500

    updated home page

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import Card from './../../Components/Card/Card'
 import Grid from '@material-ui/core/Grid';
+import Header from './../../Components/Header/Header';
+import SubHeader from './../../Components/SubHeader/subHeadre'
+import NavbarPage from './../../Components/Navbar/Navbar'
+import CarouselPage from './../../Components/Slider/Slider'
 
 class Home extends React.Component{
   render(){
     return(
+      <div>
+<Header/>
+<br/>
+<SubHeader/>
+<NavbarPage/>
+<CarouselPage/>
+      
 <div style={{margin:"5%"}}>
   
    <center>
@@ -18,6 +29,7 @@ class Home extends React.Component{
       </Grid> 
       </Grid>
     </center>   
+</div>
 </div>
     )
   }
