7b9c1c248e18162a2dd0a3b3f0760eec68f0d849
BeerbalKumar <iambeerbalkumar@gmail.com>
2020-01-09 20:04:43 +0500

    login updated

--- src/Containers/Login/Login.js
+++ src/Containers/Login/Login.js
@@ -2,40 +2,65 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
+import './Login.css';
+import { FaFacebookF,FaGoogle } from "react-icons/fa";
 
 
 export default class Login extends React.Component {
     render() {
         return (
-            <Grid container justify="center">
-                <Grid item lg={4}>
-                    <Paper>
-                        <center>
-                            <img src="https://img.icons8.com/dusk/64/000000/free-shipping.png" />
-                            <br /><br />
-                            <h1>Start</h1>
-                            <input type="text"
-                                style={{
-                                    width: "90%",
-                                    border: "1px solid grey",
-                                    padding: "2%",
-                                    borderRadius: "2px"
-                                }} />
-                            <br /><br />
-                            <input type="text"
-                                style={{
-                                    width: "90%",
-                                    border: "1px solid grey",
-                                    padding: "2%",
-                                    borderRadius: "2px"
-                                }} />
-                            <Button variant="contained" color="primary" disableElevation>
-                                Disable elevation
+            <div>
+                <br /><br /><br /><br />
+                <Grid container justify="center">
+                    <Grid item lg={4} xs={10} sm={10} md={6}>
+                        <div>
+                            <center>
+                                <img src="https://img.icons8.com/dusk/64/000000/free-shipping.png" />
+                                <br /><br />
+                                <p className="loginHeading">Log in and get to work</p>
+                                <input type="text" className="loginInput" placeholder="username" />
+                                <br /><br />
+                                <input type="text" className="loginInput" placeholder="password"/>
+                                      <br /><br />
+
+                                <Button variant="contained"
+                                    style={{
+                                        backgroundColor: "#37a000",
+                                        color: "white",
+                                        width: "90%"
+                                    }}
+                                    disableElevation>
+                                    Continue
                             </Button>
-                        </center>
-                    </Paper>
+                            <br /><br />
+
+                            <Button variant="contained"
+                                    style={{
+                                        backgroundColor: "#2d5073",
+                                        color: "white",
+                                        width: "90%"
+                                    }}
+                                    disableElevation>
+                                    Login With Facebook
+                                    <FaFacebookF style={{marginLeft:"50%"}}/>
+                            </Button>
+                           
+                            <Button variant="contained"
+                                    style={{
+                                        backgroundColor: "#c23321",
+                                        color: "white",
+                                        width: "90%"
+                                    }}
+                                    disableElevation>
+                                    Login With Google
+                                   <FaGoogle style={{marginLeft:"50%"}}/>
+                            </Button>
+                            </center>
+                            <br /><br /><br /><br />
+                        </div>
+                    </Grid>
                 </Grid>
-            </Grid>
+            </div>
         )
     }
 }
